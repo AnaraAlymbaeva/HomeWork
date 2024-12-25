043560d7121fef8dbcc6d6d0f5a3a310075942ff
Dastan <dastankeneshbekov1@gmail.com>
2024-12-26 01:20:51 +0600

    added exercise 3

--- src/test/java/my_tests/Tests.java
+++ src/test/java/my_tests/Tests.java
@@ -2,6 +2,7 @@ package my_tests;
 
 import edabit_D.Exercise1;
 import edabit_D.Exercise2;
+import edabit_D.Exercise3;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,5 +16,9 @@ public class Tests {
     @Test
     public void test2(){
         Assert.assertEquals(Exercise2.arrayOfMultiples(4,3), new int[] {4,8,12});
+    }
+    @Test
+    public void test3(){
+        Assert.assertEquals(Exercise3.otherSides(3),new double[]{3*2,3*1.73});
     }
 }
