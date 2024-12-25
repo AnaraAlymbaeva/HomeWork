eca4029e9cee4c39c6713fa1b59f3c05473657bb
Anara Alymbaeva <boardmemberbt@gmail.com>
2024-12-25 20:42:31 +0300

    Convert Minutes into Seconds

---
+++ src/test/java/edabit_Anara/ConvertMinutesIntoSeconds.java
@@ -0,0 +1,20 @@
+package edabit_Anara;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ConvertMinutesIntoSeconds {
+    public int convert(int minutes){
+        return minutes * 60;
+    }
+
+
+
+    @Test
+    void testConvert(){
+        Assert.assertEquals(convert(5), 300);
+        Assert.assertEquals(convert(3), 180);
+        Assert.assertEquals(convert(2), 120);
+
+    }
+}
